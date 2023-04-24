12b276acf19011ac0fb82506d17e84cbab38d72c
Christophe <LE1207226@crc.lennox.qc.ca>
2023-04-24 14:08:13 -0400

    Added the first python implementation.

---
+++ Project/Data/annualGlobalC02.py
@@ -0,0 +1,34 @@
+#Import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Create dataframe
+co2 = 'annual-co2-emissions-per-country.csv'
+df = pd.read_csv(co2)
+
+#Make the first graph.
+#Anual CO2 emissions per country.
+
+#Select the countries to be displayed.
+#Palce holder
+countries = ["Canada", "United States", "United Kingdom", "China", "Japan"]
+
+#Plot the dataframe with the seleced entities.
+df_plot = df[(df["Entity"].isin(countries))]
+
+#Select the style of the graph.
+plt.style.use("ggplot")
+
+fig , axs = plt.subplots() 
+
+sns.lineplot(
+    data = df_plot,
+    x="Year",
+    y="Annual CO₂ emissions",
+    hue="Entity",
+    ax=axs,
+)
+plt.title("Annual CO₂ emissions per Countries")
+plt.show()
+plt.savefig("annual CO₂ emissions per Countries")
