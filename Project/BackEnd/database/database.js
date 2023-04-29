efbebba0e73741fecd26a7c628bb1c4cdbc11fa2
Zalakaryah <zwhitcher1995@homtail.com>
2023-04-29 00:37:20 -0400

    Local Firewall blocked IPv6 connection.
    Forced IPv4 connection as a work around

--- Project/BackEnd/database/database.js
+++ Project/BackEnd/database/database.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 
 // Connection URL
 const url = process.env.MONGO_URL;
-const client = new MongoClient(url);
+const client = new MongoClient(url, {family: 4});
 
 async function connect() {
     // Use connect method to connect to the server
